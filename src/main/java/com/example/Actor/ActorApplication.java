9ff6f50669f0dd7f38ba156eb54e4fa0d4c95df1
SBR1622351 <SBR1622351@SBRMDEPP170015.local>
2019-05-26 13:36:59 +0900

    first commit

---
+++ src/main/java/com/example/Actor/ActorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Actor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorApplication.class, args);
+	}
+
+}
